058750692a66db9938df4ee17d80e1f2ea002a88
David <davidsantander9@gmail.com>
2021-07-19 21:47:39 -0500

    funtion desestructuring

---
+++ src/exercises/06-fun-desestructuring.ts
@@ -0,0 +1,27 @@
+interface Product{
+    desc: string;
+    price: number;
+}
+
+const phone: Product = {
+    desc: 'Nokia A1',
+    price: 150,
+}
+
+const tablet: Product = {
+    desc: 'iPad',
+    price: 350,
+}
+
+function calcISV( products: Product[]):[number, number] {
+    let total: number = 0;
+    products.forEach( ({ price }) => {
+        total += price;
+    })
+    return [total, total * 0.15];
+}
+
+const articles = [ phone, tablet ];
+
+const [total, isv] = calcISV(articles);
+console.log(total, isv);
