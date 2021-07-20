72f1c4834d42c7ff4100f0e8cb06164359508f8c
David <davidsantander9@gmail.com>
2021-07-19 20:36:56 -0500

    basic types

---
+++ src/exercises/01-basic-types.ts
@@ -0,0 +1,14 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let myName: string = 'David';
+let hp: number | string = 95;
+let alive: boolean = true;
+
+myName = 'Luis';
+
+hp = 'Full';
+
+console.log(myName, hp, alive);
